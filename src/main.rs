4c68dd47f0190825918e33693ea026276d700cc4
rschwalk <rschwalk@rschwalk.eu>
2020-05-17 16:37:13 +0200

    Event loop and window for SDL

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+extern crate sdl2;
+
 use std::collections::VecDeque;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
+use sdl2::pixels::Color;
+use sdl2::event::{Event, WindowEvent};
+use sdl2::keyboard::Keycode;
 
 mod networking;
 
@@ -14,15 +19,45 @@ fn main() {
         networking::listen(thread_mutex);
     });
 
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("Pi Home Display", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string()).unwrap();
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string()).unwrap();
+
+    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
     let mut running = true;
-    while running {
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..}
+                | Event::Window { win_event: WindowEvent::Close, .. }
+                | Event::KeyDown { keycode: Some(Keycode::Escape), .. }=> {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+
+        canvas.clear();
+        canvas.present();
+
         {
             let mut commands = queue_mutex.lock().unwrap();
             if !commands.is_empty() {
                 let cmd = commands.pop_front().unwrap();
                 println!("Command is: {:#?}", cmd);
                 match cmd {
-                    networking::Commands::Quit => running = false,
+                    networking::Commands::Quit => break 'running,
                     networking::Commands::Invalid => {}
                 }
             }
@@ -30,6 +65,7 @@ fn main() {
 
         thread::sleep(Duration::from_secs(1));
     }
+
     handle.join().unwrap();
 
     println!("Done");
