da9cbaa983ac6fffef67d3dfe51d92709133b0cf
rschwalk <rschwalk@rschwalk.eu>
2020-05-20 17:05:28 +0200

    Testing for cross compiling, wont work yet

--- src/main.rs
+++ src/main.rs
@@ -34,12 +34,12 @@ fn main() {
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string()).unwrap();
+    //let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string()).unwrap();
 
     let window = video_subsystem
         .window("Pi Home Display", 800, 600)
         .position_centered()
-        .opengl()
+        // .opengl()
         .build()
         .map_err(|e| e.to_string())
         .unwrap();
@@ -50,35 +50,31 @@ fn main() {
         .map_err(|e| e.to_string())
         .unwrap();
     let texture_creator = canvas.texture_creator();
-    let mut font = ttf_context.load_font("./NotoSans-Regular.ttf", 16).unwrap();
+    //let mut font = ttf_context.load_font("./NotoSans-Regular.ttf", 16).unwrap();
 
-    let surface = font
-        .render("Hello rust")
-        .blended(Color::RGBA(255, 0, 0, 255))
-        .map_err(|e| e.to_string())
-        .unwrap();
-    let texture = texture_creator
-        .create_texture_from_surface(&surface)
-        .map_err(|e| e.to_string())
-        .unwrap();
+    //let surface = font
+//         .render("Hello rust")
+//         .blended(Color::RGBA(255, 0, 0, 255))
+//         .map_err(|e| e.to_string())
+//         .unwrap();
+//     let texture = texture_creator
+//         .create_texture_from_surface(&surface)
+//         .map_err(|e| e.to_string())
+//         .unwrap();
 
     canvas.set_draw_color(Color::RGB(195, 217, 255));
     canvas.clear();
     // canvas.present();
 
-    let TextureQuery { width, height, .. } = texture.query();
-    // let padding = 64;
-    // let w = 800 - padding;
-    // let h = 600 - padding;
-    // let cx = (800 - padding) / 2;
-    // let cy = (600 - padding) / 2;
-    let target = Rect::new(64, 64, width as u32, height as u32);
+    //let TextureQuery { width, height, .. } = texture.query();
+    
+    //let target = Rect::new(64, 64, width as u32, height as u32);
 
     // let padding = 64;
     // let target = get_centered_rect(width, height, SCREEN_WIDTH - padding, SCREEN_HEIGHT - padding);
 
 
-    canvas.copy(&texture, None, Some(target)).unwrap();
+    //canvas.copy(&texture, None, Some(target)).unwrap();
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -100,7 +96,8 @@ fn main() {
             }
         }
 
-        // canvas.clear();
+        canvas.clear();
+      //  canvas.copy(&texture, None, Some(target)).unwrap();
         canvas.present();
         // std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
 
