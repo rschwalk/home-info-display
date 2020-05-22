89a4efa82030d41ec0960082aaf8fb27ee5b171b
rschwalk <rschwalk@rschwalk.eu>
2020-05-22 17:18:40 +0200

    Drawing the Frame

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,12 +1,16 @@
 extern crate sdl2;
 
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use sdl2::Sdl;
 
 pub struct MainDisplay {
-    screen_width: u32,
-    screen_height: u32,
+    // screen_width: u32,
+    // screen_height: u32,
     pub context: Sdl,
     pub canvas: sdl2::render::WindowCanvas,
+    accent_color: sdl2::pixels::Color,
+    bg_color: sdl2::pixels::Color,
 }
 
 impl MainDisplay {
@@ -21,13 +25,34 @@ impl MainDisplay {
             .build()
             .expect("Window build error!");
 
-        let mut canvas = window.into_canvas().build().expect("Canvas create error!");
+        let canvas = window.into_canvas().build().expect("Canvas create error!");
 
         Self {
-            screen_width,
-            screen_height,
+            // screen_width,
+            // screen_height,
             context: sdl_context,
             canvas,
+            accent_color: Color::RGB(235, 110, 75),
+            bg_color: Color::RGB(72, 72, 74),
         }
+    }
+
+    fn clear_with_bg(&mut self) {
+        self.canvas.set_draw_color(self.bg_color);
+        self.canvas.clear();
+    }
+
+    pub fn draw_frame(&mut self) {
+        let rects = [
+            Rect::new(392, 100, 16, 600),
+            Rect::new(0, 100, 800, 16),
+            Rect::new(340, 0, 120, 100),
+        ];
+
+        self.clear_with_bg();
+        self.canvas.set_draw_color(self.accent_color);
+        self.canvas
+            .fill_rects(&rects)
+            .expect("Not able to draw the rectangles!");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,7 @@ extern crate sdl2;
 
 use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use sdl2::rect::Rect;
-use sdl2::render::TextureQuery;
 use std::collections::VecDeque;
-use std::error::Error;
 use std::io::Write;
 use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
@@ -19,23 +15,16 @@ mod networking;
 static SCREEN_WIDTH: u32 = 800;
 static SCREEN_HEIGHT: u32 = 600;
 
-// handle the annoying Rect i32
-macro_rules! rect(
-    ($x:expr, $y:expr, $w:expr, $h:expr) => (
-        Rect::new($x as i32, $y as i32, $w as u32, $h as u32)
-    )
-);
-
 // If we are running on our development machine, then we need to send the
 // terminate message to the Tcp socket to end listening.
 #[cfg(not(target_arch = "arm"))]
 fn send_terminate() {
     match TcpStream::connect("127.0.0.1:62000") {
         Ok(mut stream) => {
-            let mut term = b"terminate\r\n";
+            let term = b"terminate\r\n";
             stream.write(&term[..]).unwrap();
         }
-        Err(e) => println!("Unable to connect to the localhost: {}", e.description()),
+        Err(e) => println!("Unable to connect to the localhost: {}", e.to_string()),
     }
 }
 
@@ -48,34 +37,34 @@ fn main() {
         networking::listen(thread_mutex);
     });
 
-    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string()).unwrap();
+    // let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string()).unwrap();
 
     let mut display = graphics::MainDisplay::new(SCREEN_WIDTH, SCREEN_HEIGHT);
-    let mut canvas = display.canvas;
+    // let mut canvas = display.canvas;
 
-    let texture_creator = canvas.texture_creator();
-    let mut font = ttf_context.load_font("./NotoSans-Regular.ttf", 16).unwrap();
+    // let texture_creator = canvas.texture_creator();
+    // let mut font = ttf_context.load_font("./NotoSans-Regular.ttf", 16).unwrap();
 
-    let surface = font
-        .render("Hello rust")
-        .blended(Color::RGBA(255, 0, 0, 255))
-        .map_err(|e| e.to_string())
-        .unwrap();
-    let texture = texture_creator
-        .create_texture_from_surface(&surface)
-        .map_err(|e| e.to_string())
-        .unwrap();
+    // let surface = font
+    //     .render("Hello rust")
+    //     .blended(Color::RGBA(255, 0, 0, 255))
+    //     .map_err(|e| e.to_string())
+    //     .unwrap();
+    // let texture = texture_creator
+    //     .create_texture_from_surface(&surface)
+    //     .map_err(|e| e.to_string())
+    //     .unwrap();
 
-    canvas.set_draw_color(Color::RGB(195, 217, 255));
-    canvas.clear();
+    // canvas.set_draw_color(Color::RGB(195, 217, 255));
+    // canvas.clear();
     // canvas.present();
 
-    let TextureQuery { width, height, .. } = texture.query();
+    // let TextureQuery { width, height, .. } = texture.query();
 
-    let target = Rect::new(64, 64, width as u32, height as u32);
+    // let target = Rect::new(64, 64, width as u32, height as u32);
 
-    canvas.copy(&texture, None, Some(target)).unwrap();
-    canvas.present();
+    // canvas.copy(&texture, None, Some(target)).unwrap();
+    // canvas.present();
 
     let mut event_pump = display.context.event_pump().unwrap();
 
@@ -100,9 +89,12 @@ fn main() {
             }
         }
 
-        canvas.clear();
-        canvas.copy(&texture, None, Some(target)).unwrap();
-        canvas.present();
+        // canvas.clear();
+        // display.canvas.clear();
+        display.draw_frame();
+        display.canvas.present();
+        // canvas.copy(&texture, None, Some(target)).unwrap();
+        // canvas.present();
 
         {
             let mut commands = queue_mutex.lock().unwrap();
